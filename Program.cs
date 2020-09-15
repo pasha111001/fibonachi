28b11b02ec118b3de31542f4e633692860504688
pasha111001 <pasha.koch11@gmail.com>
2020-09-15 19:35:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public static int Main()
+    {
+        Console.Write("Введите конец диапазона от 1 до : ");
+        int end = int.Parse(Console.ReadLine());
+        int j = 1;
+        for (int i = 1; i <= end; i += j)
+        {
+            Console.Write("{0} ", i);
+            j = i - j;
+        }
+        Console.ReadKey();
+        return 0;
+    }
+}
